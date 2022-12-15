5342b21bff75330ac6c72890d1124eddd0b12938
vanitharavichandran <119865842+vanitharavichandran@users.noreply.github.com>
2022-12-15 13:41:46 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,15 @@
+package test;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		System.out.println("Begin");
+		for(int i=100; i<1000; i++)
+		{
+			System.out.
+			println(i);
+		}
+
+	}
+
+}
